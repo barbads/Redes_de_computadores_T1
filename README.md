3c07d821502768dc28c288d477e1d1242d282899
Matheus Barbosa <matheusbmiranda@gmail.com>
2022-08-01 18:40:43 -0300

    mais passos

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Simple HTTP server
+# Simple HTTP/1.0 server
 
 usage: python3 servidor.py -b ip_addr
--- servidor.py
+++ servidor.py
@@ -2,14 +2,30 @@ import argparse
 from ast import arg
 from email import parser
 import io
+from ipaddress import ip_address
 import os
 import sys
+from http.server import *
+from turtle import end_fill
+
+# Classe handler
+class Handler(BaseHTTPRequestHandler): 
+    def post(self):
+        pass
+
+    def header():
+        pass
+
+def serve(ip):
+    SERVER_HOST = ip
+    SERVER_PORT = 9999 #Porta escolhida
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-b')
     arguments = parser.parse_args()
-    print(arguments.b)
+    ip_addr = arguments.b
+    print(ip_address)
 
 if __name__ == '__main__':
     main()
